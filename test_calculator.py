df678a7c0a2a4493761409040bd47219d0e144f7
nilofar4499 <nilofar4499@email.com>
2026-08-09 09:11:22 +0530

    change in code

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply, even
 
 
 def test_add():
@@ -7,3 +7,15 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+
+def test_even():
+    assert even(4) == "even"
+
+
+def test_odd():
+    assert even(7) == "odd"
