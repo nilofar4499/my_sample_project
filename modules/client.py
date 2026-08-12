6ae17b27567b4b38cb232244eb1011a3131823e4
nilofar4499 <nilofar4499@email.com>
2026-08-12 17:41:39 +0530

    new module add in that i learn how to import parameters from different files

---
+++ modules/client.py
@@ -0,0 +1,13 @@
+# import operations
+# operations.add(122,200)
+#
+# operations.sub(200,1)
+
+
+# from operations import add,sub
+# add(122,200)
+# sub(200,1)
+
+from operations import*
+add(122,200)
+sub(200,1)
